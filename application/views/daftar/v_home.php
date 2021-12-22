d8bd2c2607e9bc5b5312eeff8b1f2dc7c16c0d7f
afissabi <59241963+afissabi@users.noreply.github.com>
2021-12-22 20:18:38 +0700

    asd

--- application/views/daftar/v_home.php
+++ application/views/daftar/v_home.php
@@ -1,5 +1,5 @@
 <div class="page-section banner-seo-check">
-    <div class="wrap bg-image" style="background-image: url(../assets/img/bg_pattern.svg);">
+    <div class="wrap bg-image" style="background-image: url(assets/img/bg_pattern.svg);">
     <div class="container text-center">
         <div class="row justify-content-center wow fadeInUp">
         <div class="col-lg-8">
@@ -15,6 +15,37 @@
     </div>
     </div>
 </div>
+
+<!-- Banner info -->
+<div class="page-section banner-info">
+    <div class="wrap bg-image" style="background-image: url(assets/img/bg_pattern.svg);">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3 pr-lg-5 wow fadeInUp">
+            <h2 class="title-section">Tentang Klinik <br></h2>
+            <div class="divider"></div>
+            <p>We're an experienced and talented team of passionate consultants who breathe with search engine marketing.</p>
+            
+            <ul class="theme-list theme-list-light text-white">
+              <li>
+                <div class="h5">SEO Content Strategy</div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+              </li>
+              <li>
+                <div class="h5">B2B SEO</div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 py-3 wow fadeInRight">
+            <div class="img-fluid text-center">
+              <img src="assets/img/banner_image_2.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- .wrap -->
+  </div> <!-- .page-section -->
 
 <div class="page-section">
     <div class="container">
